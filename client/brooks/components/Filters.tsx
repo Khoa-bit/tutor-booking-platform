fb826a54218eef727af10a7937fe6ad7c18ee455
Minh Do <quangminh.do152@gmail.com>
2022-05-09 15:46:38 +0700

    first commit

---
+++ client/brooks/components/Filters.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/solid'
+
+import Filter from "./Filter"
+
+export default function filters() {
+    return (
+        <div className="">
+            <div className="flex h-10">
+                <Filter />
+                <Filter />
+                <Filter />
+                <button type="button"
+                        className="h-11 inline-flex items-center justify-center rounded-md border border-transparent bg-primary-default px-4 py-2 text-sm font-medium text-white hover:bg-primary-dark focus:outline-none focus-visible:ring-primary-default "
+                        >Apply</button>
+            </div>
+            
+        </div>
+    );
+}
--- client/brooks/tailwind.config.js
+++ client/brooks/tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('../img/hero-image.png')",
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'black': '#000000',
+  
+        'primary-default': '#094FA0',
+        'primary-dark': '#002E65',
+        'primary-light': '#E9F3FF',
+  
+        'secondary-default': '#FFC700',
+  
+        'grey-dark': '#636769',
+        'grey-default': '#8B8F92',
+        'grey-light': '#DADBDC',
+        'grey-lighter': '#F4F5F6',
+      },
+    },
+   
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
