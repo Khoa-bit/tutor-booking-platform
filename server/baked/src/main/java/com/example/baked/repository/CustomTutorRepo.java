b20a00d9a454311943365bd5e42700df590aa690
RexeonNova <hoclamhuynh@gmail.com>
2022-05-17 19:03:54 +0700

    save

--- server/baked/src/main/java/com/example/baked/repository/CustomTutorRepo.java
+++ server/baked/src/main/java/com/example/baked/repository/CustomTutorRepo.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 import com.example.baked.model.Tutor;
 
-public interface CustomRepo {
-    List<Tutor> getTutorOnFeatures(String a, String b, String c);
+public interface CustomTutorRepo {
+    List<Tutor> getTutorOnFeatures(String major, String qualification, String subject);
 }
