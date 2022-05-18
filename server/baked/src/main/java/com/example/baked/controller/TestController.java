0588c2cd9b5ab8f97b7b3598e09a4f5015f3e0b2
RexeonNova <hoclamhuynh@gmail.com>
2022-05-18 16:45:42 +0700

    test

--- server/baked/src/main/java/com/example/baked/controller/TestController.java
+++ server/baked/src/main/java/com/example/baked/controller/TestController.java
@@ -11,12 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class TestController {
     
     @Autowired
-    private static CustomTutorRepoImp tutorRepo;
+    private CustomTutorRepo tutorRepo;
     
     @GetMapping(value = "/")
     public void homepage() {
