f7bcf8d6069fd13bdeb714e35ace88436602b1f6
RexeonNova <hoclamhuynh@gmail.com>
2022-05-31 14:52:43 +0700

    filter 2

--- server/baked/src/main/java/com/example/baked/repo/CustomTutorRepo.java
+++ server/baked/src/main/java/com/example/baked/repo/CustomTutorRepo.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface CustomTutorRepo {
   List<AuthUser> getTutorOnMainSearch(String city, String subject, String grade);
 
-  List<AuthUser> getTutorOnMainSearch2(String city, String subject, String grade);
+  List<AuthUser> getTutorOnMainSearch2(String city, String subject, String grade, String district);
 
   List<AuthUser> getTutorOnPopularity();
 }
